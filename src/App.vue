2dfedf07f3f1b525c896cab9735fa7e489ce3175
wangxiang <909980020@qq.com>
2018-12-28 19:33:42 +0800

    初始化构建工具

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div >
+    <router-view/>
+    <FooterGuide/>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  components : {
+    FooterGuide
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped="">
+
+</style>
